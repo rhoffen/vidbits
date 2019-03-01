07696a9fac61e1eaa707358b710d1e4cd2426175
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-03-01 12:51:57 -0800

    Time to drop to server layer.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,16 @@
 const {assert} = require('chai');
+
+describe('User visits landing page', () => {
+  describe('with no existing videos', () => {
+    it('shows no videos', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#videos-container'),'');
+    });
+  });
+
+  it('can navigate to add a video', () => {
+    browser.url('/');
+    browser.click('a[href="./videos/create.html"]');
+    assert.include(browser.getText('body'),'Save a video');
+  });
+});
